0a30d4a9429ff203e20bc35c457c3b4ea409a81b
mattiagaggi <m.gaggi@hotmail.it>
2025-01-02 18:41:49 +0100

    chat history

--- src/main.py
+++ src/main.py
@@ -6,12 +6,15 @@ from langchain.agents.format_scratchpad.openai_tools import (
     format_to_openai_tool_messages,
 )
 from langchain.agents import tool
+from langchain_core.messages import AIMessage, HumanMessage
 
 from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
 
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain.agents import AgentExecutor
+MEMORY_KEY = "chat_history"
+
 
 
 
@@ -23,6 +26,10 @@ def get_word_length(word: str) -> int:
     return len(word)
 
 tools = [get_word_length]
+
+chat_history = []
+
+
 
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
@@ -37,6 +44,7 @@ prompt = ChatPromptTemplate.from_messages(
         ),
         ("user", "{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
+        MessagesPlaceholder(variable_name=MEMORY_KEY),
     ]
 )
 
@@ -46,15 +54,24 @@ agent = (
         "agent_scratchpad": lambda x: format_to_openai_tool_messages(
             x["intermediate_steps"]
         ),
+         "chat_history": lambda x: x["chat_history"],
     }
     | prompt
     | llm_with_tools
     | OpenAIToolsAgentOutputParser()
 )
 
-
+input1="How many letters in the word eudca"
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, return_intermediate_steps=True)
-result=agent_executor({"input": "How many letters in the word eudca"})
+result=agent_executor({"input": input1,"chat_history": chat_history})
+
+chat_history.extend(
+    [
+        HumanMessage(content=input1),
+        AIMessage(content=result["output"]),
+    ]
+)
+
 print("Intermediate Steps:")
 for step in result["intermediate_steps"]:
     print(step)
